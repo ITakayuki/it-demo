b7d2a827a8df5d1dee325496875d38e7144047b7
t.inoue <takayuki.inoue@i-studio.co.jp>
2022-09-07 15:33:09 +0900

    修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+const title = `ITakayuki Prototypes`;
+
 const html = `
   <h1>Hello, World!</h1>
 `
 
+document.head.insertAdjacentHTML("afterbegin", `<title>${title}</title>`);
 document.querySelector("#app")!.insertAdjacentHTML("afterbegin", html);
